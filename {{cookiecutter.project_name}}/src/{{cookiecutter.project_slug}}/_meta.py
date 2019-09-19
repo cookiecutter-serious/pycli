49cc5c1d1210674748e2f2cf2e3777e655270dab
Yan QiDong <yanqd0@outlook.com>
2019-09-20 06:58:49 +0800

    Generate year

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/_meta.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/_meta.py
@@ -3,4 +3,4 @@
 __author__ = '{{ cookiecutter.full_name }}'
 __email__ = '{{ cookiecutter.email }}'
 __home__ = 'https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_name }}'
-__copyright__ = 'Copyright (C) 2017 ' + __author__
+__copyright__ = 'Copyright (C) {{ cookiecutter.year }} ' + __author__
